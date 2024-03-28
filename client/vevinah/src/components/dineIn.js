27dd8bd9f4d90072f0f39d4e4dfd7bce01a17811
L-JM3214 <lactorjm4@gmail.com>
2024-03-29 02:12:45 +0300

    Lazy Load

--- client/vevinah/src/components/dineIn.js
+++ client/vevinah/src/components/dineIn.js
@@ -1,11 +1,13 @@
-import React from "react";
+// Import useState
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUtensils } from "@fortawesome/free-solid-svg-icons";
 import "../App.css";
 import { Link } from "react-router-dom";
 
-
 const DineIn = () => {
+  const [videoLoaded, setVideoLoaded] = useState(false);
+
   return (
     <div className="dine-in-section">
       <div className="video-card">
@@ -13,19 +15,21 @@ const DineIn = () => {
           title="Restaurant Video"
           width="580"
           height="315"
-          src="https://player.vimeo.com/external/173249177.sd.mp4?s=4b45afd27c8a45058186028705247db86c9b381b&profile_id=164&oauth2_token_id=57447761"
+          src={videoLoaded ? "https://player.vimeo.com/external/173249177.sd.mp4?s=4b45afd27c8a45058186028705247db86c9b381b&profile_id=164&oauth2_token_id=57447761" : ""}
+          style={{ display: videoLoaded ? "block" : "none" }}
+          onLoad={() => setVideoLoaded(true)}
           allowFullScreen
         ></iframe>
       </div>
       <div className="dine-in-description">
         <FontAwesomeIcon icon={faUtensils} className="utensils" />
         <h2>DINE IN</h2>
-        <p style={{color:"black"}}>
+        <p style={{ color: "black" }}>
           Savor the essence of culinary bliss in our cozy ambiance. Join us for
           an unforgettable DINE IN experience!
         </p>
         <Link to="/dine-in">
-          <button className="reservation-button"> Book a reservation</button>
+          <button className="reservation-button">Book a reservation</button>
         </Link>
       </div>
     </div>
@@ -33,3 +37,4 @@ const DineIn = () => {
 };
 
 export default DineIn;
+
